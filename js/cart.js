e2551d55ddaa64b7ec246a411effb998ebdea296
vidhikabathani <vidhikabathani110@gmail.com>
2023-08-24 12:24:14 +0530

    project-2

--- js/cart.js
+++ js/cart.js
@@ -1,37 +1,99 @@
 import nav from "../components/nav.js";
 
-document.getElementById("nav").innerHTML = nav()
+document.getElementById("nav").innerHTML=nav()
 
-const display = (data) => {
-  data.map((product) => {
-    let img = document.createElement("img");
-    img.src = product.image;
 
-    let title = document.createElement("h2");
-    title.innerHTML = product.title;
+let cart = JSON.parse(localStorage.getItem("cart")) || [];
+let totalprice = 0;
+cart.map((ele) => {
+  totalprice += ele.price * ele.qty * 10;
+});
 
-    let price = document.createElement("h4");
-    price.innerHTML = product.price;
+let show = (cart) => {
+  cart.map((ele, index) => {
+    let maindiv = document.createElement("div");
 
-    let category = document.createElement("p");
-    category.innerHTML = product.category;
+    let imgdiv = document.createElement("div");
 
-    let div = document.createElement("div")
-    div.append(img, title, price, category)
+    let image = document.createElement("img");
+    image.src = ele.image;
 
-    document.getElementById("cart-p").append(div)
-  })
-  let btn = document.createElement("button");
-  btn.innerHTML = "CHECK OUT";
-  
-  document.getElementById("btn").append(btn)
-}
+    let texts = document.createElement("div");
 
-let get = async () => {
-  let res = await fetch("http://localhost:3000/cart");
-  let data = await res.json();
-  display(data);
-  console.log(data)
+    let title = document.createElement("h3");
+    title.innerHTML = ele.title;
+
+    
+    let price1 = document.createElement("p");
+    price1.innerHTML = `${ele.price * 10}$`;
+    
+    let plusminse = document.createElement("div");
+    
+    let minse = document.createElement("button");
+    minse.innerHTML = "-";
+    
+    let noq = document.createElement("button");
+    noq.innerHTML = ele.qty;
+
+    let plus = document.createElement("button");
+    plus.innerHTML = "+";
+
+    imgdiv.append(image);
+    texts.append(title, price1);
+    plusminse.append(minse, noq, plus);
+    maindiv.append(imgdiv, texts, plusminse);
+    document.getElementById("cartpage").append(maindiv);
+
+    price1.setAttribute("class", "price1");
+    maindiv.setAttribute("class", "maindiv");
+    imgdiv.setAttribute("class", "imgdiv");
+    texts.setAttribute("class", "texts");
+    plusminse.setAttribute("class", "plusminse");
+
+    minse.addEventListener("click", () => {
+      console.log(ele);
+      let qty = cart[index].qty;
+      if (qty == 1) {
+        cart.splice(index, 1);
+        localStorage.setItem("cart", JSON.stringify(cart));
+
+        console.log(cart);
+        window.location.reload();
+      } else {
+        cart[index].qty -= 1;
+        localStorage.setItem("cart", JSON.stringify(cart));
+        window.location.reload();
+      }
+    });
+    plus.addEventListener("click", () => {
+      console.log("test");
+      cart[index].qty += 1;
+      localStorage.setItem("cart", JSON.stringify(cart));
+      window.location.reload();
+    });
+  });
 };
+show(cart);
 
-get();
+let show2 = () => {
+  let maindiv2 = document.createElement("div");
+
+  let lable2 = document.createElement("p");
+  lable2.innerHTML = "TOTAL PRICE:";
+
+  let btn2=document.createElement("button");
+  btn2.innerHTML="Check Out"
+
+  let disprice = document.createElement("p")
+
+
+  maindiv2.append(lable2);
+  document.getElementById("other").append(maindiv2,btn2);
+  document.getElementById("price").append(lable2, totalprice);
+
+  maindiv2.setAttribute("class", "maindiv2");
+
+};
+show2();
+
+
--- js/product.js
+++ js/product.js
@@ -1,64 +1,96 @@
 import nav from "../components/nav.js";
-document.getElementById("nav").innerHTML = nav()
 
-const display = (data) => {
-    data.map((product) => {
+let show = (data) => {
+    console.log(data);
+    document.getElementById("box2").innerHTML = "";
+    data.map((ele) => {
+        let div = document.createElement("div");
         let img = document.createElement("img");
-        img.src = product.image;
-
-        let title = document.createElement("h2");
-        title.innerHTML = product.title;
-
-        let price = document.createElement("h4");
-        price.innerHTML = product.price;
-
+        img.src = ele.image;
+        let title = document.createElement("h3");
+        title.innerHTML = ele.title;
         let category = document.createElement("p");
-        category.innerHTML = product.category;
-
+        category.innerHTML = ele.category;
+        category.setAttribute("class", "category")
+        let price = document.createElement("p");
+        price.innerHTML = `${ele.price * 10}$`;
+        category.setAttribute("class", "price")
         let rate = document.createElement("p");
-        rate.innerHTML = product.rating.rate;
-
-        let btn = document.createElement("button")
-        btn.innerHTML = "Buy Now"
-        btn.setAttribute("class", "btn")
-
-
-        let div = document.createElement("div")
-        div.append(img, title, price, category, rate, btn)
-
-        document.getElementById("box2").append(div)
-
-
-        btn.addEventListener("click", () => {
-            console.log(product.id)
-            if (localStorage.getItem("Login")) {
-                fetch("http://localhost:3000/cart", {
-                    method: "POST",
-                    headers: { 'content-type': 'application/json' },
-                    body: JSON.stringify(product)
-                })
+        rate.innerHTML = ele.rating.rate;
+        category.setAttribute("class", "rate")
+        let btn1 = document.createElement("button");
+        btn1.innerHTML = "BUY NOW";
+        btn1.setAttribute("class", "btn1")
+        // let btn2=document.createElement("button");
+        // btn2.innerHTML="BUY NOW";
+        // divbtn=document.createElement("div")
+        // divbtn.append(btn1,btn2);
+        // divbtn.setAttributes("class","buttons");
+        div.append(img, title, category, price, rate, btn1);
+        document.getElementById("box2").append(div);
+        btn1.addEventListener("click", () => {
+            let cart = JSON.parse(localStorage.getItem("cart")) || [];
+            let exists = false;
+            console.log(cart);
+            cart.map((item, index) => {
+                if (item.id == ele.id) {
+                    cart[index].qty += 1;
+                    localStorage.setItem("cart", JSON.stringify(cart));
+                    exists = true;
+                }
+            });
+            if (!exists) {
+                cart.push({ ...ele, qty: 1 });
+                localStorage.setItem("cart", JSON.stringify(cart));
+                alert("added to cart");
+                window.location.reload();
             }
-            else {
-                alert("please first login then you can add to cart")
-                setTimeout(
-                    window.location.href = "../pages/login.html"
-                    , 1000)
-            }
-        })
-    })
-}
 
-const get = () => {
-    fetch(" http://localhost:3000/product")
-        .then((response) => response.json())
-        .then((response) => display(response));
-}
+            // function addToCart(id){
+            //   if(!data[id].itemInCart){
+            //     cartList=[...cartList,data[id]];
+            //     show();
 
-get();
-// const cart = fetch("http://localhost:3000/cart", {
-//     method: "POST",
-//     headers: {
-//         "Content-Type": "application/json",
-//     },
-//     body: JSON.stringify(cart),
-// });
+            //   }
+            // }
+
+
+
+        });
+
+    });
+};
+let products = []
+
+
+document.getElementById("lth").addEventListener("click", () => {
+    products.sort((a, b) => a.price - b.price)
+    show(products)
+})
+
+
+document.getElementById("htl").addEventListener("click", () => {
+
+    console.log("clicked");
+    products.sort((a, b) => b.price - a.price)
+    show(products)
+})
+document.getElementById("men").addEventListener("click", () => {
+    let temp = products.filter((val) => val.category == "men's clothing")
+    show(temp)
+})
+document.getElementById("wm").addEventListener("click", () => {
+    let temp = products.filter((val) => val.category == "women's clothing")
+    show(temp)
+})
+document.getElementById("ele").addEventListener("click", () => {
+    let temp = products.filter((val) => val.category == "electronics")
+    show(temp)
+})
+
+
+fetch(" http://localhost:3000/product")
+    .then((response) => response.json())
+    .then((data) => { products = data, show(data) });
+
+document.getElementById("nav").innerHTML = nav();
